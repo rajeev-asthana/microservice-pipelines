4c1a6002b2dd36055d5f3f3a7a969405b6d7e554
rajeev-asthana <68969240+rajeev-asthana@users.noreply.github.com>
2021-04-25 20:45:09 +0530

    Update jenkinsJob.groovy
    Moved the stage 'Maven Environment Setup' to individual methods.

--- vars/jenkinsJob.groovy
+++ vars/jenkinsJob.groovy
@@ -3,15 +3,6 @@ def call(){
         stage('Checkout') {
             checkout scm
         }
-        
-        stage('Maven Environment Setup'){
-           sh '''
-              export MAVEN_HOME=/var/jenkins_home/apache-maven/apache-maven-3.8.1
-              export PATH=$PATH:$MAVEN_HOME/bin
-              mvn --version
-              mvn clean package
-              '''
-        }
 
         // Execute different stages depending on the job
         if(env.JOB_NAME.contains("deploy")){
@@ -23,12 +14,32 @@ def call(){
 }
 
 def packageArtifact(){
+    
+     stage('Maven Environment Setup'){
+           sh '''
+              export MAVEN_HOME=/var/jenkins_home/apache-maven/apache-maven-3.8.1
+              export PATH=$PATH:$MAVEN_HOME/bin
+              mvn --version
+              mvn clean package
+              '''
+        }
+    
     stage("Package artifact") {
         sh "mvn package"
     }
 }
 
 def buildAndTest(){
+    
+    stage('Maven Environment Setup'){
+           sh '''
+              export MAVEN_HOME=/var/jenkins_home/apache-maven/apache-maven-3.8.1
+              export PATH=$PATH:$MAVEN_HOME/bin
+              mvn --version
+              mvn clean package
+              '''
+        }
+    
     stage("Backend tests"){
         sh "mvn test"
     }
