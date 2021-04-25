d9b8677ef2b13bc706394685246d12d870ee1256
rajeev-asthana <68969240+rajeev-asthana@users.noreply.github.com>
2021-04-25 20:28:33 +0530

    Update jenkinsJob.groovy
    Corrected MAVEN_HOME path in 'Maven Environment Setup'  Stage

--- vars/jenkinsJob.groovy
+++ vars/jenkinsJob.groovy
@@ -6,7 +6,7 @@ def call(){
         
         stage('Maven Environment Setup'){
            sh '''
-              export MAVEN_HOME=/opt/maven
+              export MAVEN_HOME=/var/jenkins_home/apache-maven/apache-maven-3.8.1
               export PATH=$PATH:$MAVEN_HOME/bin
               mvn --version
               mvn clean package
